1d78bd78bb53ce1d0caab2ad44307a563981bd0c
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-11-30 23:22:48 +0530

    Add project files.

---
+++ Demo/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Class1
+    {
+        public static double FindMean(int[] a, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += a[i];
+            return (double)sum / (double)n;
+        }
+        public static double FindMedian(int[] a, int n)
+        {
+            Array.Sort(a);
+            if (n % 2 != 0)
+                return (double)a[n / 2];
+            return (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0;
+        }
+        public static double Findmode(int[] a, int n)
+        {
+            int maxValue = 0, maxCount = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                int count = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (a[j] == a[i])
+                        ++count;
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxValue = a[i];
+                }
+            }
+            return maxValue;
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+            int n = a.Length;
+            Console.Write("Mean = " + Class1.FindMean(a, n) + "\n");
+            Console.Write("Median = " + Class1.FindMedian(a, n) + "\n");
+            Console.Write("Median = " + Class1.Findmode(a, n) + "\n");
+        }
+    }
+}
