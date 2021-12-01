dadd2ad7cb6fbfa28fd460ff68310a3e65712abf
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-12-01 17:11:15 +0530

    OOPS

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,13 +8,26 @@ namespace Demo
 {
     internal class Program
     {
+       static int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+
         public static void Main(string[] args)
         {
-            int[] a = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
-            int n = a.Length;
-            Console.Write("Mean = " + Class1.FindMean(a, n) + "\n");
-            Console.Write("Median = " + Class1.FindMedian(a, n) + "\n");
-            Console.Write("Median = " + Class1.Findmode(a, n) + "\n");
+            var sobject = new Stats();
+            sobject.Numbers = a;
+
+            var sum = sobject.CalSum();
+            Console.WriteLine($"Sum:{sum}");
+
+            var mean = sobject.CalMean();
+            Console.WriteLine($"Mean:{mean}");
+
+            var median = sobject.CalMedian();
+            Console.WriteLine($"Median:{median}");
+
+            var mode = sobject.CalMode();
+            Console.WriteLine($"Mode:{mode}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Demo/Stats.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Stats
+    {
+        internal int[] Numbers { private get; set; }
+
+        internal int CalSum()
+        {
+            int sum = 0;
+            foreach (int i in Numbers)
+            {
+                sum += i;
+            }
+            // this uses a nifty util from .NET from the LINQ namespace
+            // see the "using..." section at the top of the file
+            return sum;
+        }
+        internal double CalMean()
+        {
+            int n = Numbers.Length;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += Numbers[i];
+
+            return (double)sum / (double)n;
+        }
+
+        internal double CalMedian()
+        {
+            int n = Numbers.Length;
+            // Checks if input array has even or odd number of elements and finds the average
+            if (n % 2 != 0)
+                return (double)Numbers[n / 2];
+
+            // Gets position of the median
+            return (double)(Numbers[(n - 1) / 2] + Numbers[n / 2]) / 2.0;
+        }
+
+        internal int CalMode()
+        {
+            int n = Numbers.Length;
+            // stores max value of input array
+            int max = Numbers.Max();
+
+
+            int t = max + 1;
+            int[] count = new int[t];
+            for (int i = 0; i < t; i++)
+                count[i] = 0;
+
+            // Stores count of each element of input array
+            for (int i = 0; i < n; i++)
+                count[Numbers[i]]++;
+
+            // mode is the index with maximum count
+            int mode = 0;
+            int k = count[0];
+            for (int i = 1; i < t; i++)
+            {
+                if (count[i] > k)
+                {
+                    k = count[i];
+                    mode = i;
+                }
+            }
+
+            return mode;
+
+        }
+    }
+}
